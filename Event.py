9cecc90dac10ead9d26021d0c379eef52c2a5990
haanmiba <haanmiba@gmail.com>
2018-11-10 19:53:50 -0500

    Added functionality for printing events into console.

--- Event.py
+++ Event.py
@@ -7,10 +7,7 @@ class Event:
         self.end = end
 
     def __str__(self):
-        return '{}\n{}\nStart: {}\nEnd: {}\n'.format(self.title,
-                                                     self.link,
-                                                     self.start.strftime('%m/%d/%Y %I:%M %p %Z%z'),
-                                                     self.end.strftime('%m/%d/%Y %I:%M %p %Z%z'))
+        return 'Title: {}\nLink: {}\nStart: {}\nEnd: {}\n\n'.format(self.title, self.link, self.start, self.end)
 
     def __repr__(self):
         return self.__str__()
