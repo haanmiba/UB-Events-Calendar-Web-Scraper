e22e1a0d418a7af9b10be348ee91a483afa872c9
haanmiba <haanmiba@gmail.com>
2018-11-17 00:34:05 -0500

    Added overwrite setting and command line config

--- Configuration.py
+++ Configuration.py
@@ -1,7 +1,7 @@
 class Configuration:
 
-    def __init__(self, path, headless, deep_scrape, start_page, end_page,
-                 all_pages, export, export_path, export_extension, print_events):
+    def __init__(self, path, headless, deep_scrape, start_page, end_page, all_pages,
+                 export, overwrite, export_path, export_extension, print_events):
         self.chromedriver_path = path
         self.headless = headless
         self.deep_scrape = deep_scrape
@@ -9,6 +9,7 @@ class Configuration:
         self.end_page = end_page
         self.all_pages = all_pages
         self.export = export
+        self.overwrite = overwrite
         self.export_path = export_path
         self.export_extension = export_extension
         self.print_events = print_events
