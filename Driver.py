6b7db0d6dfdb51be55d61250925f5838d8286727
haanmiba <haanmiba@gmail.com>
2018-11-17 05:37:59 -0500

    Began adding commented documentation

--- Driver.py
+++ Driver.py
@@ -3,22 +3,28 @@ from Utility import (read_config_file, read_args,
                      InvalidConfigFileTypeError, InvalidConfigFileValueError,
                      OverwriteExistingFileError, print_events, export_events)
 from UBEventsCalendarScraper import UBEventsCalendarScraper
-from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import WebDriverException, TimeoutException
 from urllib3.exceptions import MaxRetryError
 
 
+# Usage message that represents the correct command line usage.
 USAGE_STR = '''usage: python Driver.py --config <path>
 usage: python Driver.py --path <driver_path> (--head) (--deep) (--print) ([<last_page> | <first_page> <last_page> | --all]) (--export <export_path>) (--overwrite)'''
 
+# Set of allowed command line arguments.
 ALLOWED_ARGS = {'--config', '--path', '--head', '--deep', '--print', '--all', '--export', '--overwrite'}
 
 
 def main():
+    """Main method to scrape data from the University at Buffalo Events Calendar."""
+
+    # Check if the user input less than 3 command line arguments. If so, terminate the program with exit code 1.
     if len(sys.argv) < 3:
         print('This program requires at least 3 command line arguments.')
         print(USAGE_STR)
         sys.exit(1)
 
+    # Check if the user used an invalid argument. If so, terminate the program with exit code 1.
     invalid_args = set(filter(lambda x: x.startswith('--'), sys.argv)) - ALLOWED_ARGS
     if invalid_args:
         print('`{}` is not a valid argument.'.format(next(iter(invalid_args))))
@@ -27,28 +33,44 @@ def main():
 
     exit_code = 0
     try:
+        # If the second command line argument is `--config`, create configurations from a config file.
         if sys.argv[1] == '--config':
             config = read_config_file(sys.argv[2])
+        # Else, if the second command line argument is `--path`, create configurations from the command line argmuents.
         elif sys.argv[1] == '--path':
             config = read_args(sys.argv)
-        scraper = UBEventsCalendarScraper(config)
-        events = scraper.scrape_events()
+
+        scraper = UBEventsCalendarScraper(config)  # Initialize a web scraper for the UB Events Calendar.
+        events = scraper.scrape_events()           # Scrape events from the UB Events Calendar.
+
+        # Export the scraped events if the config allows exporting.
         if config.export:
             export_events(events, config)
+
+        # Print out the scraped events if the config allows printing.
         if config.print_events:
             print_events(events)
-    except (InvalidConfigFileTypeError, InvalidConfigFileValueError) as e:
+
+    # Handle exceptions that deal with issues with the configuration file or overwriting an existing file.
+    except (InvalidConfigFileTypeError, InvalidConfigFileValueError, OverwriteExistingFileError) as e:
         print('{}: {}'.format(e.__class__.__name__, str(e)))
         exit_code = 1
-    except (WebDriverException, OverwriteExistingFileError) as e:
+
+    # Handle exceptions that deal with issues with the web scraper.
+    except (WebDriverException, TimeoutException) as e:
         scraper.quit()
         print('{}: {}'.format(e.__class__.__name__, str(e)))
         exit_code = 1
+
+    # Handle exceptions that deal with network issues.
     except MaxRetryError:
         scraper.quit()
         print('Failed to establish a new connection with https://calendar.buffalo.edu/. Check network connection.')
         exit_code = 2
+
+    # Terminate program with `exit_code`.
     sys.exit(exit_code)
+
 
 if __name__ == '__main__':
     main()
--- UBEventsCalendarScraper.py
+++ UBEventsCalendarScraper.py
@@ -9,28 +9,69 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 class UBEventsCalendarScraper(Scraper):
+    """
+    Web scraper for the University at Buffalo Events Calendar
+
+    Attributes
+    ----------
+    config : Configuration
+        configuration settings for the web scraper
+    event_list : list
+        list of events scraped from the University at Buffalo Events Calendar
+
+    Methods
+    -------
+    deep_scrape(evt)
+        `deep scrape` a single event -- scrape data from that event's web page
+    scrape_events()
+        scrape events from the University at Buffalo Events Calendar based upon the configuration settings
+    next_page_button_exists()
+        sees whether or not a next page button exists
+    click_next_page_button()
+        click on the next page button
+    """
 
     def __init__(self, config):
+        """
+        Parameters
+        ----------
+        config : Configuration
+            Configuration settings for the web scraper
+        """
+
         Scraper.__init__(self, config.chromedriver_path, config.headless)
         self.open_url('https://calendar.buffalo.edu/', 'list-event')
         self.config = config
         self.event_list = []
 
     def deep_scrape(self, evt):
+        """`Deep scrape` a single event -- scrape data from that event's web page.
+
+        Parameters
+        ----------
+        evt : Event
+            The event that will have its data scraped
+        """
+
+        # Open the event's hyperlink in a new tab
         self.open_url(evt.link, 'accordion-header-link', new_tab=True)
 
+        # Scrape the event's description
         description_elems = self.browser.find_elements_by_xpath(".//div[@itemprop='description']")
         if description_elems:
             evt.description = description_elems[0].text
 
+        # Scrape the event's location
         location_elems = self.browser.find_elements_by_xpath(".//section[@itemprop='location']/p")
         if location_elems:
             evt.location = location_elems[0].text
 
+        # Scrape the event's contact information
         contact_elems = self.browser.find_elements_by_xpath(".//section[@class='event-detail-contact-person']/p")
         if contact_elems:
             evt.contact = extract_contact_info(contact_elems[0].text)
 
+        # Scrape any additional information about the event
         additional_info_labels = self.browser.find_elements_by_xpath(".//div[@class='custom-field-label']")
         additional_info_values = self.browser.find_elements_by_xpath(".//div[@class='custom-field-value']")
         if additional_info_labels and additional_info_values:
@@ -38,45 +79,78 @@ class UBEventsCalendarScraper(Scraper):
             for label, value in zip(additional_info_labels, additional_info_values):
                 evt.additional_info[re.sub(':', '', label.text)] = value.text
 
+        # Close the tab
         self.close_tab()
 
     def scrape_events(self):
+        """Scrape events from the University at Buffalo Events Calendar based upon the configuration settings.
+
+        Returns
+        -------
+        list
+            a list of events that were scraped
+        """
+
         current_page = 0
+        # While the web scraper has not reached the end page or finished looking at all pages, scrape events
         while current_page < self.config.end_page or self.config.all_pages:
 
-            if self.config.start_page <= current_page:
+            # If the current page is at or after the page to begin scraping, scrape the page
+            if current_page >= self.config.start_page:
+                # Get all events on the current page
                 event_elements = self.browser.find_elements_by_class_name('list-event-preview')
                 for event_elem in event_elements:
+                    # Extract the header, event hyperlink, start time, end time, and initialize a new Event
                     header = event_elem.find_element_by_xpath('.//h3/a')
                     date_time = event_elem.find_element_by_xpath('.//p')
                     start, end = extract_date_time(date_time.text, tz='US/Eastern')
                     evt = Event(header.text, header.get_attribute('href'), start, end)
 
+                    # If configuration settings enable deep scraping, deep scrape this event
                     if self.config.deep_scrape:
                         self.deep_scrape(evt)
 
+                    # Append this scraped event to the event list
                     self.event_list.append(evt)
 
+            # If a next page button does not exist, stop scraping
             if not self.next_page_button_exists():
                 break
 
-            button = self.get_next_page_button()
-            button.click()
+            # Click the next page button
+            self.click_next_page_button()
+
+            # Have the ChromeDriver wait until an element with class name `list_event` loads
             wait = WebDriverWait(self.browser, self.timeout)
             wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'list-event')))
 
+            # Increment the current page the web scraper is on
             current_page += 1
 
+        # Return the event list
         return self.event_list
 
     def next_page_button_exists(self):
+        """Sees whether or not a next page button exists.
+
+        Returns
+        -------
+        bool
+            True -- if a next page button exists
+            False -- otherwise
+        """
+
         try:
             self.browser.find_element_by_class_name('icon-angle-right')
         except NoSuchElementException:
             return False
         return True
 
-    def get_next_page_button(self):
+    def click_next_page_button(self):
+        """Click on the next page button."""
+
+        # Get the next page button
         button_child = self.browser.find_element_by_class_name('icon-angle-right')
         button = button_child.find_element_by_xpath('..')
-        return button
+
+        button.click()  # Click on the next page button
